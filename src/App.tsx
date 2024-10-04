60524aaa9d7fe6a29f0fa266113239f4b8afdc2a
Workshop Participant - AJR <awsalain@amazon.com>
2024-10-04 19:55:57 -0400

    Added Auth Components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import '@aws-amplify/ui-react/styles.css';
-import { ThemeProvider } from '@aws-amplify/ui-react';
+import { ThemeProvider, Authenticator } from '@aws-amplify/ui-react';
 import { Category, Recommended, Header, Footer, Product } from './components';
 
 function App() {
-  const baseURL = process.env.NODE_ENV === 'development' ? (process.env.NEXT_PUBLIC_LOCAL ? '/RetailStore' : `/proxy/8081/RetailStore`) : '';
+  
+  // Print process.env.NODE_ENV
+  console.log('NODE_ENV: ', process.env.NODE_ENV);
+  console.log('NEXT_PUBLIC_LOCAL: ', process.env.NEXT_PUBLIC_LOCAL);
+  
+  // const baseURL = process.env.NODE_ENV === 'development' ? `/proxy/8081/RetailStore` : '';
+  // const baseURL = process.env.NODE_ENV === 'development' ? (process.env.NEXT_PUBLIC_LOCAL ? '/RetailStore' : `/proxy/8081/RetailStore`) : ''
+  const baseURL = process.env.NODE_ENV === 'development' ? "/RetailStore" : `/proxy/8081/RetailStore`;
+
+  // Print baseURL
+  console.log('baseURL: ', baseURL);
 
   return (
     <ThemeProvider>
+      <Authenticator.Provider>
         <Router basename={baseURL}>
           <Header />
           <Routes>
@@ -19,6 +30,7 @@ function App() {
           </Routes>
           <Footer />
         </Router>
+      </Authenticator.Provider>
     </ThemeProvider>
   );
 }
